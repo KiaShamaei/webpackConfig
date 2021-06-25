4f1185389a978ee1727b2854dd12aa8c89c01503
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-06-25 18:14:14 +0430

    step 2 add ts loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,43 +2,45 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry:path.resolve(__dirname,"..", "./src/index.tsx"),
+    
+	 entry: path.resolve(__dirname, '..', './src/index.tsx'),
+
+	
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
     },
+    devServer: { contentBase: path.join(__dirname, "src") },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)?$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [
-                    {loader:"babel-loader"}
-                ],
+                use: ["babel-loader"],
             },
-            // {
-                
-            //     test: /\.(ts|tsx)$/,
-            //     exclude: /node_modules/,
-            //     use: ["ts-loader"],
-            // },
-            // {
-            //     test: /\.(css|scss)$/,
-            //     use: ["style-loader", "css-loader"],
-            // },
-            // {
-            //     test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-            //     use: ["file-loader"],
-            // },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			  }
         ],
     },
-    output: { 
-        path: path.join(__dirname,"..", "build"), 
-        filename: "bundle.js",
-     },
-    mode: process.env.NODE_ENV || "development",
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "..", "./src/index.html"),
+            template: path.join(__dirname, "../src", "index.html"),
         }),
     ],
 };
